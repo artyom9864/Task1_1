a8defcc5c3c966b8a9640edf84e545341eac6681
Артём Братчук <artyom9864@mail.ru>
2021-01-05 20:41:27 +0300

    Добавьте файлы проекта.

---
+++ Task1_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using StringLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length==0)
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            string username = args[0];
+
+            var helloMessage = new HelloMessage();
+            var message = helloMessage.GetHelloMessage(username);
+
+            Console.WriteLine(message);
+        }
+    }
+}
